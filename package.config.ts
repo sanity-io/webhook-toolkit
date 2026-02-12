10203db8cc9c9dde03e66da25e0b3bdc6798d5e7
Espen Hovlandsdal <espen@hovlandsdal.com>
2026-02-12 08:46:00 -0800

    chore: upgrade dependencies, esm-first, move lint/release config

--- package.config.ts
+++ package.config.ts
@@ -1,12 +1,6 @@
 import {defineConfig} from '@sanity/pkg-utils'
 
 export default defineConfig({
-  extract: {
-    rules: {
-      'ae-forgotten-export': 'error',
-      'ae-incompatible-release-tags': 'off',
-      'ae-missing-release-tag': 'off',
-    },
-  },
+  extract: {rules: {'ae-incompatible-release-tags': 'off', 'ae-missing-release-tag': 'off'}},
   tsconfig: 'tsconfig.build.json',
 })
